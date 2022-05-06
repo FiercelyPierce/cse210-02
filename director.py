6e7e396baf370c93570d05a94b45e456b3f238f9
FiercelyPierce <cir21001@byui.edu>
2022-05-05 19:05:14 -0700

    Updated the list of methods to be used

--- director.py
+++ director.py
@@ -7,13 +7,14 @@ from deck import ____
 def main():
     current_points = calculate_points()
 
-    while current_points > 0 and :
+    # Runs the program until the player either runs out of points or cards
+    while current_points > 0 and ____:
         pass
 
 # Calculates the current points the player has
 def calculate_points():
     pass
 
-# 
+# Prompts the player to guess whether the next number will be Higher (H), or Lower (L)
 def guess_prompt():
     pass
