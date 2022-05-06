a66a0461eced11a0eba43024a663291ad93b7126
FiercelyPierce <cir21001@byui.edu>
2022-05-05 19:04:32 -0700

    Made a rough skeleton of methods to be used

--- director.py
+++ director.py
@@ -1,5 +1,19 @@
+'''
 
+'''
+
+from deck import ____
 
 def main():
+    current_points = calculate_points()
+
+    while current_points > 0 and :
+        pass
+
+# Calculates the current points the player has
+def calculate_points():
     pass
 
+# 
+def guess_prompt():
+    pass
