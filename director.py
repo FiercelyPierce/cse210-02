34d6637b9409ac7bf777da125df628c57d2e675e
FiercelyPierce <cir21001@byui.edu>
2022-05-07 11:29:01 -0700

    Made it a class and connected the deck class to it

--- director.py
+++ director.py
@@ -2,67 +2,60 @@
 
 '''
 import random
-from deck import ____
+from deck import Deck
 
-
-'''def main():
-    current_points = calculate_points()
-
-    # Runs the program until the player either runs out of points or cards
-    while current_points > 0 and ____:
-        pass
-
-# Calculates the current points the player has
-def calculate_points():
-    pass    
-
-# Prompts the player to guess whether the next number will be Higher (H), or Lower (L)
-def guess_prompt():
-    pass
-'''
+# cse210
+# Team 4 - Spring2022
+# 
+# Pierce Cirks
+# Matthew Scoville
+# Anita Taylor
+# Andrew Vargas
 
 print()
 user_points = 300
-card = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 
- # Loop tha game until user get 0 points
-def main():
-    while user_points != 0:
-        guess()
-    print("Game over")
-    
+class Director:
 
-# Prompts the player to guess whether the next number will be Higher (H), or Lower (L)    
-def guess():
-    card_number = random.choice(card)
-    print(f"The card is {card_number}")
-    user_choise = input("Higher or lower (h/l)  ")
-    next_number = random.choice(card)
-    print(f"Next card was: {next_number}")
+    # Loop the game until user get 0 points
+    def main(self):
+        while user_points > 0:
+            self.guess()
+            
+        print("Game over")
+        
 
-    if next_number > card_number and user_choise == "h":
-        correct_asnwer()
-    elif next_number < card_number and user_choise == "h":
-        incorrect_answer()
-    elif next_number < card_number and user_choise == "l":
-        correct_asnwer()
-    elif next_number > card_number and user_choise == "l":
-        incorrect_answer()
-    else:
-        print("Try again")
+    # Prompts the player to guess whether the next number will be Higher (H), or Lower (L)    
+    def guess(self):
+        card_number = Deck.drawCard()
+        print(f"The card is {card_number}")
+        user_choice = input("Higher or lower (H/L)  ")
+        next_number = Deck.drawCard()
+        print(f"Next card was: {next_number}")
 
-
-# Calculates the current points the player has
-def correct_asnwer():
-    correct = user_points + 100
-    new_points = correct
-    print(f"your score is {new_points}")
-    print()
+        if next_number > card_number and user_choice.lower() == "h":
+            self.correct_asnwer()
+        elif next_number < card_number and user_choice.lower() == "h":
+            self.incorrect_answer()
+        elif next_number < card_number and user_choice.lower() == "l":
+            self.correct_asnwer()
+        elif next_number > card_number and user_choice.lower() == "l":
+            self.incorrect_answer()
+        else:
+            print("There seems to be a typo. Please try again")
 
 
-def incorrect_answer():
-    incorrect = user_points - 75
-    new_points = incorrect
-    print(f"your score is {new_points}")
-    print()
-main()
+    # Calculates the current points the player has
+    def correct_asnwer(self):
+        new_points = user_points + 100
+        print(f"your score is {new_points}")
+        print()
+
+
+    def incorrect_answer(self):
+        new_points = user_points - 75
+        print(f"your score is {new_points}")
+        print()
+
+
+    main()
